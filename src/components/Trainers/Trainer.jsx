239236ee8325f5afe313a464723dcd5ddcf18fe7
okturan <okanert@gmail.com>
2024-10-12 19:04:39 +0300

    style fix

--- src/components/Trainers/Trainer.jsx
+++ src/components/Trainers/Trainer.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 const Trainer = ({ name, specialty, image, alt }) => {
   return (
-    <div className="trainer-card">
-      <picture>
-        <img src={image} alt={alt} className="trainer-image" />
-        <div className="trainer__info">
-          <h3 className="trainer__name">{name}</h3>
-          <p className="trainer__specialty">{specialty}</p>
-        </div>
-        <div className="trainer__decoration"></div>
-      </picture>
-    </div>
+    <picture>
+      <img src={image} alt={alt} className="trainer-image" />
+      <div className="trainer__info">
+        <h3 className="trainer__name">{name}</h3>
+        <p className="trainer__specialty">{specialty}</p>
+      </div>
+      <div className="trainer__decoration"></div>
+    </picture>
   );
 };
 
